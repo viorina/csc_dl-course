d434b0ca15420e800ad7c289f22a65980d4cafd8
Ekaterina Babshukova <ekaterina.babshukova@yandex.ru>
2019-05-20 16:16:17 +0300

    add task 1

---
+++ assignment1/linear_classifer.py
@@ -0,0 +1,194 @@
+import numpy as np
+
+
+def softmax(predictions):
+    '''
+    Computes probabilities from scores
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+
+    Returns:
+      probs, np array of the same shape as predictions - 
+        probability for every class, 0..1
+    '''
+    # TODO implement softmax
+    # Your final implementation shouldn't have any loops
+    max_pred = np.max(predictions, axis=-1, keepdims=True)
+    return np.exp(predictions - max_pred) / np.sum(np.exp(predictions - max_pred), axis=-1, keepdims=True)
+
+
+def cross_entropy_loss(probs, target_index):
+    '''
+    Computes cross-entropy loss
+
+    Arguments:
+      probs, np array, shape is either (N) or (batch_size, N) -
+        probabilities for every class
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+
+    Returns:
+      loss: single value
+    '''
+    # TODO implement cross-entropy
+    # Your final implementation shouldn't have any loops
+    return -np.log(np.choose(target_index, probs.T)).mean()
+
+
+def softmax_with_cross_entropy(predictions, target_index):
+    '''
+    Computes softmax and cross-entropy loss for model predictions,
+    including the gradient
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+
+    Returns:
+      loss, single value - cross-entropy loss
+      dprediction, np array same shape as predictions - gradient of predictions by loss value
+    '''
+    # TODO implement softmax with cross-entropy
+    # Your final implementation shouldn't have any loops
+    probs = softmax(predictions)
+    loss = cross_entropy_loss(probs, target_index)
+    dprediction = probs
+
+    n, ind_probs = None, None
+    if type(target_index) == int:
+        n = 1
+        ind_probs = target_index
+    else:
+        n = target_index.shape[0]
+        ind_probs = (range(n), target_index)
+    dprediction[ind_probs] -= 1
+    dprediction /= n
+
+    return loss, dprediction
+
+
+def l2_regularization(W, reg_strength):
+    '''
+    Computes L2 regularization loss on weights and its gradient
+
+    Arguments:
+      W, np array - weights
+      reg_strength - float value
+
+    Returns:
+      loss, single value - l2 regularization loss
+      gradient, np.array same shape as W - gradient of weight by l2 loss
+    '''
+
+    # TODO: implement l2 regularization and gradient
+    # Your final implementation shouldn't have any loops
+    loss = reg_strength * np.sum(W ** 2)
+    grad = 2 * reg_strength * W
+
+    return loss, grad
+    
+
+def linear_softmax(X, W, target_index):
+    '''
+    Performs linear classification and returns loss and gradient over W
+
+    Arguments:
+      X, np array, shape (num_batch, num_features) - batch of images
+      W, np array, shape (num_features, classes) - weights
+      target_index, np array, shape (num_batch) - index of target classes
+
+    Returns:
+      loss, single value - cross-entropy loss
+      gradient, np.array same shape as W - gradient of weight by loss
+
+    '''
+    predictions = np.dot(X, W)
+
+    # TODO implement prediction and gradient over W
+    # Your final implementation shouldn't have any loops
+    loss, grad = softmax_with_cross_entropy(predictions, target_index)
+    dW = np.dot(X.T, grad)
+    
+    return loss, dW
+
+
+class LinearSoftmaxClassifier():
+    def __init__(self):
+        self.W = None
+
+    def fit(self, X, y, batch_size=100, learning_rate=1e-7, reg=1e-5,
+            epochs=1):
+        '''
+        Trains linear classifier
+        
+        Arguments:
+          X, np array (num_samples, num_features) - training data
+          y, np array of int (num_samples) - labels
+          batch_size, int - batch size to use
+          learning_rate, float - learning rate for gradient descent
+          reg, float - L2 regularization strength
+          epochs, int - number of epochs
+        '''
+
+        num_train = X.shape[0]
+        num_features = X.shape[1]
+        num_classes = np.max(y)+1
+        if self.W is None:
+            self.W = 0.001 * np.random.randn(num_features, num_classes)
+
+        loss_history = []
+        for epoch in range(epochs):
+            shuffled_indices = np.arange(num_train)
+            np.random.shuffle(shuffled_indices)
+            sections = np.arange(batch_size, num_train, batch_size)
+            batches_indices = np.array_split(shuffled_indices, sections)
+
+            # TODO implement generating batches from indices
+            # Compute loss and gradients
+            # Apply gradient to weights using learning rate
+            # Don't forget to add both cross-entropy loss
+            # and regularization!
+            for batch_ind in batches_indices:
+                batch_X, batch_y = X[batch_ind], y[batch_ind]
+
+                loss, grad = linear_softmax(batch_X, self.W, batch_y)
+                loss_l2, grad_l2 = l2_regularization(self.W, reg)
+
+                self.W -= learning_rate * (grad + grad_l2)
+                loss_history.append(loss + loss_l2)
+
+            # end
+            print("Epoch %i, loss: %f" % (epoch, loss))
+
+        return loss_history
+
+    def predict(self, X):
+        '''
+        Produces classifier predictions on the set
+       
+        Arguments:
+          X, np array (test_samples, num_features)
+
+        Returns:
+          y_pred, np.array of int (test_samples)
+        '''
+        y_pred = np.zeros(X.shape[0], dtype=np.int)
+
+        # TODO Implement class prediction
+        # Your final implementation shouldn't have any loops
+        y_pred = np.argmax(X.dot(self.W), axis=-1)
+
+        return y_pred
+
+
+
+                
+                                                          
+
+            
+
+                
---
+++ assignment1/metrics.py
@@ -0,0 +1,57 @@
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = 0
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    TP, FP, FN, TN = 0, 0, 0, 0
+    N = len(prediction)
+
+    for (y_pred, y) in zip(prediction, ground_truth):
+        if y_pred and y:
+            TP += 1
+        if y_pred and not y:
+            FP += 1
+        if not y_pred and y:
+            FN += 1
+        if not y_pred and not y:
+            TN += 1
+
+    if (TP + FP):
+        precision = TP / (TP + FP)
+    if TP + FN:
+        recall = TP / (TP + FN)
+    if precision + recall:
+        f1 = 2 * precision * recall / (precision + recall)
+    accuracy = (TP + TN) / N
+    
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    return sum(y_pred == y for (y_pred, y) in zip(prediction, ground_truth)) / len(prediction)
